38423d42f62f1cdd9c16c01d7165ada6446f6b2d
Gabriel Vieira <luizgabrielvieiraleal.dev@gmail.com>
2022-09-27 10:22:32 -0300

    prosseguindo com o analizador sintático

--- sin.rb
+++ sin.rb
@@ -8,6 +8,7 @@ class Sin
   def initialize(filename)
     @filename = filename
     @lex = Lex.new(@filename)
+    @current_token = nil
     @errors = []
   end
 
@@ -22,43 +23,76 @@ class Sin
   private
 
   def program_validate
-    token = @lex.next_token
-    if token.type == Patterns::KEYWORDS[:TYPE] && token.value.downcase == 'class'
+    next_token
+    if @current_token.type == Patterns::KEYWORDS[:TYPE] && @current_token.value.downcase == 'class'
+      next_token
       class_validate
     else
-      @errors.push("Unexpected token #{token.value} in file #{@filename}")
+      @errors.push("Unexpected token #{@current_token.value} in file #{@filename}")
     end
   end
 
   def class_validate
-    token = @lex.next_token
-    if token.type == Patterns::TYPE_ID[:TYPE]
-      token = @lex.next_token
-      if token.type == Patterns::KEYWORDS[:TYPE] && token.value.downcase == 'inherits'
-        token = @lex.next_token
-        if token.type == Patterns::TYPE_ID[:TYPE]
-          token = @lex.next_token
-          token.type == Patterns::OPEN_SCOPE[:TYPE] ? feature_validate : @errors.push("Unexpected token #{token.value} in file #{@filename}")
+    while @current_token && @current_token.value.downcase != 'class'
+      if @current_token.type == Patterns::TYPE_ID[:TYPE]
+        next_token
+        if @current_token.type == Patterns::KEYWORDS[:TYPE] && @current_token.value.downcase == 'inherits'
+          next_token
+          if @current_token.type == Patterns::TYPE_ID[:TYPE]
+            next_token
+            if @current_token.type == Patterns::OPEN_SCOPE[:TYPE]
+              next_token
+              feature_validate
+            else
+              @errors.push("Unexpected token #{@current_token.value} in file #{@filename}")
+            end
+          else
+            @errors.push("Unexpected token #{@current_token.value} in file #{@filename}")
+          end
+        elsif @current_token.type == Patterns::OPEN_SCOPE[:TYPE]
+          next_token
+          feature_validate
         else
-          @errors.push("Unexpected token #{token.value} in file #{@filename}")
+          @errors.push("Unexpected token #{@current_token.value} in file #{@filename}")
         end
-      elsif token.type == Patterns::OPEN_SCOPE[:TYPE]
-        feature_validate
       else
-        @errors.push("Unexpected token #{token.value} in file #{@filename}")
+        @errors.push("Unexpected token #{@current_token.value} in file #{@filename}")
       end
-    else
-      @errors.push("Unexpected token #{token.value} in file #{@filename}")
+      next_token
     end
   end
 
   def feature_validate
-    token = @lex.next_token
-    if token.type == Patterns::OBJECT_ID[:TYPE]
-      token = @lex.next_token
-      puts token.inspect
-    else
-      @errors.push("Unexpected token #{token.value} in file #{@filename}")
+    loop do
+      if @current_token.type == Patterns::OBJECT_ID[:TYPE]
+        next_token
+        if @current_token.type == Patterns::OPEN_EXP[:TYPE]
+          next_token
+          formal_validate
+        elsif @current_token.type == Patterns::PUNCT[:TYPE] && @current_token.value == ':'
+          next_token
+          if @current_token.type == Patterns::TYPE_ID[:TYPE]
+            next_token
+
+          else
+            @errors.push("Unexpected token #{@current_token.value} in file #{@filename}")
+          end
+        else
+          @errors.push("Unexpected token #{@current_token.value} in file #{@filename}")
+        end
+      else
+        @errors.push("Unexpected token #{@current_token.value} in file #{@filename}")
+      end
+      next_token
+      break if @current_token.type == Patterns::OBJECT_ID[:TYPE]
     end
+  end
+
+  def formal_validate
+    puts @current_token.inspect
+  end
+
+  def next_token
+    @current_token = @lex.next_token
   end
 end
